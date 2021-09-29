d692f4cfbffbf71afc0cc7f98db707f2f3e2ccdf
Viacheslav Zavodchenkov <agconagconovich@gmail.com>
2021-09-29 17:57:12 +0300

    Init java project

---
+++ src/ru/agcon/iep0221/Main.java
@@ -0,0 +1,12 @@
+package ru.agcon.iep0221;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello world!!!");
+        for (double i = 0; i <= 100; i++){
+            System.out.println(Math.pow(i, 12));
+        }
+
+    }
+}
